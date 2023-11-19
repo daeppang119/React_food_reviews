e9f426950698b13383cf14760aa54476adf503f1
daeppang119 <dbsk9436@naver.com>
2023-11-20 08:35:48 +0900

    main 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import Router from "./shared/Router";
-import { GlobalStyle } from "./GlobalStyle";
 
 export default function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Router />
-    </>
-  );
+  return <></>;
 }
--- src/GlobalStyle.js
+++
@@ -1,7 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-export const GlobalStyle = createGlobalStyle`
-    body{scrollbar-width: none;
-    &::-webkit-scrollbar {
-      display: none;
-    }}
-`;
